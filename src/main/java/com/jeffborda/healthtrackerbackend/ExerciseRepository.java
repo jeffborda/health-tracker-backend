bc71e1dc813d58e64caf1ada895882bb17f90647
Jeff Borda <jeffborda@outlook.com>
2019-01-17 10:34:46 -0800

    post and get routes working

---
+++ src/main/java/com/jeffborda/healthtrackerbackend/ExerciseRepository.java
@@ -0,0 +1,5 @@
+package com.jeffborda.healthtrackerbackend;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExerciseRepository extends CrudRepository<Exercise, Long> { }
